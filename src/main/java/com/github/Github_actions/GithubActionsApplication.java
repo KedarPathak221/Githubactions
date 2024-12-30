af7e8c329ce36ad77bef17bc0ba817a957074837
KedarPathak221 <kdar2020@gmail.com>
2024-12-30 18:03:54 +0530

    code

--- src/main/java/com/github/Github_actions/GithubActionsApplication.java
+++ src/main/java/com/github/Github_actions/GithubActionsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class GithubActionsApplication {
-	
+
 	@GetMapping("/message")
 	public String getMessage()
 	{
